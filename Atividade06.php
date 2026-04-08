fdc8c0b9dd0328e6e9e4f7cda14fae042b2ee8e6
Eduardo Senac <senaceduardo80@gmail.com>
2026-04-08 09:52:58 -0300

    tabuada 7

---
+++ Atividade06.php
@@ -0,0 +1,18 @@
+<?php 
+
+$numero = 7;
+
+echo "Tabuada do $numero:<br>";
+echo "$numero x 1 = " . ($numero * 1) . "<br>";
+echo "$numero x 2 = " . ($numero * 2) . "<br>";
+echo "$numero x 3 = " . ($numero * 3) . "<br>";
+echo "$numero x 4 = " . ($numero * 4) . "<br>";
+echo "$numero x 5= " . ($numero * 5) . "<br>";
+echo "$numero x 6 = " . ($numero * 6) . "<br>";
+echo "$numero x 7 = " . ($numero * 7) . "<br>";
+echo "$numero x 8 = " . ($numero * 8) . "<br>";
+echo "$numero x 9 = " . ($numero * 9) . "<br>";
+echo "$numero x 10 = " . ($numero * 10) . "<br>";
+
+
+?>
